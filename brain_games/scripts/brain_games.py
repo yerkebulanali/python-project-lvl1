dd2cd64a9cad44f8b4fd06713c647480f29b7812
Yerkebulan <yerkebulanali@gmail.com>
2020-04-10 11:41:12 -0400

    Upload after step 3, howecer there are some issues about publishing package into pypi

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,2 +1,12 @@
+from brain_games.cli import welcome_user
+
+
 def main():
-    print("Welcome to the Brain Games!")
+	print("Welcome to the Brain Games!")
+	print()
+	greet = welcome_user()	
+	print(greet)
+
+
+
+

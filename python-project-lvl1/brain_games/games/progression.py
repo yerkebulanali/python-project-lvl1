1b478d7c44182c66aa8d4c58328950aa976525f5
Yerkebulan <yerkebulanali@gmail.com>
2020-05-09 04:37:37 -0400

    Third comment edited from mentor

---
+++ python-project-lvl1/brain_games/games/progression.py
@@ -0,0 +1,16 @@
+import random
+DESCRIPTION = "What number is missing in the progression?"
+
+
+def play_round():
+    start_choice = random.randrange(50)
+    progression = random.randrange(1, 11)
+    index = random.randrange(10)
+    result = []
+    for i in range(1, 11):
+        start_choice += progression
+        result.append(str(start_choice))
+    answer = result[index]
+    result[index] = '..'
+    question = ' '.join(result)
+    return question, answer
---
+++ python-project-lvl1/brain_games/scripts/brain_even.py
@@ -0,0 +1,9 @@
+from brain_games import engine, games
+
+
+def main():
+    engine.run(games.even)
+
+
+if __name__ == '__main__':
+    main()
